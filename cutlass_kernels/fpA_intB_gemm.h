9a98af05d2c6802ad99ed97ebc3a6b461986d96b
Wuwei Lin <wuwei@apache.org>
2023-11-16 15:32:36 -0800

    cleanup

--- cutlass_kernels/fpA_intB_gemm.h
+++ cutlass_kernels/fpA_intB_gemm.h
@@ -20,8 +20,6 @@ using uint4b_t = cutlass::uint4b_t;
 void preprocess_weights(int8_t* preprocessed_quantized_weight, const int8_t* row_major_quantized_weight, size_t rows,
     size_t cols, bool is_int4, int arch);
 
-// TODO: Support more general bias shape
-
 template <typename WeightType, cutlass::WeightOnlyQuantOp QuantOp>
 void gemm_fp16_int_bias_act(const half* A, const WeightType* B, const half* weight_scales, const half* bias, half* C,
     std::optional<std::string> activation, int m, int n, int k, int group_size, int bias_stride, char* workspace_ptr,
@@ -32,6 +30,5 @@ void gemm_fp16_int_bias_act_residual(const half* A, const WeightType* B, const h
     const half* residual, half* C, const std::string& activation, const std::string& binary_op,
     const std::string& unary_op, int m, int n, int k, int group_size, char* workspace_ptr, size_t workspace_bytes,
     cudaStream_t stream);
-
 
 } // namespace fastertransformer
